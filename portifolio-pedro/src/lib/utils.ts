9cfa41ca4afefdd12e0377a0d54c1204bd4f9742
Pedro Del Col <pedroavgdelcol@gmail.com>
2025-09-10 18:27:45 -0300

    reset

--- portifolio-pedro/src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Une classNames e resolve conflitos do Tailwind
-export function cn(...inputs: any[]) {
-  return twMerge(clsx(inputs));
-}
